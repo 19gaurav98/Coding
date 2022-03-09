92bb6abf04c2fd33081bbd7b761449b62a59c60c
19gaurav98 <35462960+19gaurav98@users.noreply.github.com>
2022-03-09 12:14:23 +0530

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,10 @@
+public boolean equals(Object o) {
+    if (o == this)
+        return true;
+    if (!(o instanceof Money))
+        return false;
+    Money other = (Money)o;
+    boolean currencyCodeEquals = (this.currencyCode == null && other.currencyCode == null)
+      || (this.currencyCode != null && this.currencyCode.equals(other.currencyCode));
+    return this.amount == other.amount && currencyCodeEquals;
+}
